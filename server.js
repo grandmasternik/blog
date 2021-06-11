e9295f1d1b466320aa055f5abb9bf11d0b93ac48
Nikki Mallory <nikkimallory13@gmail.com>
2021-06-11 09:38:04 -0500

    add home page

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3000;
 // Initialize Express
 const app = express();
 
+// Home Route
+app.get("/", (req, res) => res.render('index.ejs'));
+
 // Tells express to listen
 app.listen(port, () => {
     console.log(`listening ${port}`);
